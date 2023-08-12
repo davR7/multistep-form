ada531a0b8202d45976ff62637429fa7b8b9aea2
Davidson Ratis <davidsonmakarios@gmail.com>
2023-08-12 19:48:22 -0300

    created - UserSignup Page

---
+++ src/pages/UserSignup/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './style';
+import MultiStepForm from '../../components/MultiStepForm';
+
+function UserSignup() {
+  return (
+    <S.UserSignup>
+      <MultiStepForm />
+    </S.UserSignup>
+  );
+}
+
+export default UserSignup;
---
+++ src/pages/UserSignup/style.ts
@@ -0,0 +1,3 @@
+import styled from 'styled-components';
+
+export const UserSignup = styled.div``;
