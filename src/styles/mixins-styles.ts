9c8f48950ad72b6003e87778f6979318888daba3
Davidson Ratis <davidsonmakarios@gmail.com>
2023-08-12 19:18:08 -0300

    created - mixins (styled-components)

---
+++ src/styles/mixins-styles.ts
@@ -0,0 +1,14 @@
+import { css } from 'styled-components';
+
+export const FlexRow = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FlexColumn = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
