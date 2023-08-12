cba0a6632c17ddd50fed2759eaa3aa68d34eb1f5
Davidson Ratis <davidsonmakarios@gmail.com>
2023-08-12 18:39:05 -0300

    Created - types (global types)

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export type UserProps = {
+  email: string;
+  password: string;
+  passConfirm: string;
+  firstName: string;
+  lastName: string;
+  userName: string;
+  currentCompany: string;
+  designation: string;
+  startDate: string;
+};
